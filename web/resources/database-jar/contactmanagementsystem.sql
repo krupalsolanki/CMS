cd4641ebd87b1a8708bbb021ddc8f8e3181786a4
Jay <Jay@Jay>
2013-06-24 11:30:57 +0530

    with new database but not showing the auto suggestion

--- web/resources/database-jar/contactmanagementsystem.sql
+++ web/resources/database-jar/contactmanagementsystem.sql
@@ -1,124 +1,64 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jun 03, 2013 at 05:03 AM
--- Server version: 5.1.68
--- PHP Version: 5.3.4
+CREATE TABLE IF NOT EXISTS employee
+(
+	empId int AUTO_INCREMENT Primary Key,
+	empEmailId varchar(78) unique,
+	password varchar(30)
+);
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+CREATE TABLE IF NOT EXISTS category
+(
+	categoryId int AUTO_INCREMENT Primary Key,
+	categoryName varchar(15)
+);
 
+CREATE TABLE IF NOT EXISTS contacts (
+  contactId int(11) NOT NULL AUTO_INCREMENT,
+  firstName varchar(30) NOT NULL,
+  lastName varchar(30) NOT NULL,
+  email varchar(78) DEFAULT NULL,
+  phoneNo varchar(20) DEFAULT NULL,
+  companyName varchar(100) DEFAULT NULL,
+  companyLoc varchar(50) DEFAULT NULL,
+  designation varchar(120) DEFAULT NULL,
+  linkedInUrl varchar(256) DEFAULT NULL,
+  categoryId int DEFAULT NULL,
+  PRIMARY KEY (contactId),
+  FOREIGN KEY(categoryId) references category(categoryId)
+) ;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+CREATE TABLE IF NOT EXISTS contactList (
+  contactListId int(11) NOT NULL AUTO_INCREMENT,
+  empId int(11),
+  contactId int(11),
+  PRIMARY KEY (contactListId),
+  FOREIGN KEY (empId) References employee(empId),
+  FOREIGN KEY(contactId) References contacts(contactId)
+); 
 
---
--- Database: `contactmanagementsystem`
---
+CREATE TABLE IF NOT EXISTS contactrelation (
+  contactRelationId int(11) NOT NULL AUTO_INCREMENT,
+  contactListId int(11),
+  nickName varchar(30),
+  rating int(11) DEFAULT NULL,
+  notes varchar(300) DEFAULT NULL,
+  personalMsg varchar(300),
+  PRIMARY KEY (contactRelationId),
+  FOREIGN KEY (contactListId) References contactList(contactListId)
+  
+);
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS interests(
+  interestId int(11) NOT NULL AUTO_INCREMENT,
+  interestName varchar(50) DEFAULT NULL,
+  PRIMARY KEY (interestId)
+);
 
---
--- Table structure for table `contactlist`
---
-
-CREATE TABLE IF NOT EXISTS `contactlist` (
-  `contactListId` int(11) NOT NULL AUTO_INCREMENT,
-  `myId` int(11) DEFAULT NULL,
-  `contactId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`contactListId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `contactlist`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `contactrelation`
---
-
-CREATE TABLE IF NOT EXISTS `contactrelation` (
-  `contactRelationId` int(11) NOT NULL AUTO_INCREMENT,
-  `contactlistId` int(11) DEFAULT NULL,
-  `rating` int(11) DEFAULT NULL,
-  `notes` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`contactRelationId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `contactrelation`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `contacts`
---
-
-CREATE TABLE IF NOT EXISTS `contacts` (
-  `contactId` int(11) NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(30) DEFAULT NULL,
-  `lastName` varchar(30) DEFAULT NULL,
-  `email` varchar(78) DEFAULT NULL,
-  `phoneNo` varchar(20) NOT NULL,
-  `companyName` varchar(100) DEFAULT NULL,
-  `companyLoc` varchar(50) DEFAULT NULL,
-  `designation` varchar(120) DEFAULT NULL,
-  `linkedInUrl` varchar(256) NOT NULL,
-  `notes` char(255) NOT NULL,
-  `addedBy` char(30) DEFAULT NULL,
-  PRIMARY KEY (`contactId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=114 ;
-
---
--- Dumping data for table `contacts`
---
-
-INSERT INTO `contacts` (`contactId`, `firstName`, `lastName`, `email`, `phoneNo`, `companyName`, `companyLoc`, `designation`, `linkedInUrl`, `notes`, `addedBy`) VALUES
-(109, 'Walter', 'Coutinho', 'waltery2w@gmail.com', '(091)-9860633725', 'compassites', 'Pune', 'CEO', '', '', 'Jay Patel'),
-(110, 'Satyam', 'Singh', 'satyam.mgs@gmail.com', '(091)-9860633725', 'compassites', 'Pune', 'ID card Collector', '', '', 'Jay Patel'),
-(111, 'Krupal', 'Solanki', 'krupalsolanki@live.com', '(123)-1231231223', 'compassites', 'Pune', 'CEO', '', '', 'Aapka Pyaara Satyam'),
-(112, 'Jay', 'Patel', 'darpan_jay@yahoo.in', '(911)-7521346543', 'compassites', 'Pune', 'ID card Collector', '', '', 'Krupal'),
-(113, 'ruchika', 'sharma', 'ruchika.geca@gmail.com', '(091)-2547896314', 'compassites', 'banglore', 'QA', 'http://in.linkedin.com/pub/jay-butani/60/926/695', 'mad girl..', 'Jay Patel');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `interestbridge`
---
-
-CREATE TABLE IF NOT EXISTS `interestbridge` (
-  `interestId` int(11) DEFAULT NULL,
-  `contactId` int(11) DEFAULT NULL,
-  `IB_id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`IB_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `interestbridge`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `interests`
---
-
-CREATE TABLE IF NOT EXISTS `interests` (
-  `interestId` int(11) NOT NULL AUTO_INCREMENT,
-  `interestName` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`interestId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `interests`
---
+CREATE TABLE IF NOT EXISTS interestbridge (
+  IB_id int(11) NOT NULL AUTO_INCREMENT,
+  interestId int(11),
+  contactId int(11),
+  PRIMARY KEY (IB_id),
+  FOREIGN KEY (interestId) References interests(interestId),
+  FOREIGN KEY (contactId)  References contacts(contactId)
+ );
 
